2a5e394a3149be7a857dd542913466ca0de00dc3
CaioLima <caiohenrys50@gmail.com>
2026-08-12 02:46:34 -0300

    app project

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (request, response) => {
+  response.json({ message: "Olá Caio lima" })
+})
+
+export { app }
