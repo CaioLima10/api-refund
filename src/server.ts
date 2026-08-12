61aceb34080e24470fe7ca9ecd235ce7f55a0eb5
CaioLima <caiohenrys50@gmail.com>
2026-08-12 04:08:06 -0300

    feat: add Zod schema for environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "@/app"
+import { env } from "./env"
 
-const PORT = 3331
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(env.PORT, 
+  () => console.log(`Server is running on port ${env.PORT}`
+))
