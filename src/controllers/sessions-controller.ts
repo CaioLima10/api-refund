eb4257d6a3f3f0d4b8ddc0e5f9293b1c10143dc9
CaioLima <caiohenrys50@gmail.com>
2026-08-12 16:47:55 -0300

    feat: create session user

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,44 @@
+import { configAuth } from "@/configs/auth";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/app-error";
+import { compare } from "bcrypt";
+import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
+import { z } from "zod";
+
+class SessionsController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().trim().email({ message: "E-mail inválido" }),
+      password: z.string().min(6)
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({ where: { email } })
+
+    if(!user) {
+      throw new AppError("E-mail ou senha inválido.", 401)
+    }
+    
+    const passwordMatched = await compare(password, user.password)
+    
+    if(!passwordMatched) {
+      throw new AppError("E-mail ou senha inválido.", 401)
+    }
+
+    const {  secret, expiresIn } = configAuth.jwt
+
+    const token = sign({ role: user.role  }, secret, {
+      subject: user.id,
+      expiresIn
+    })
+
+    const {password: _, ...userWithoutPassword} = user
+
+    return response.status(200).json({token, userWithoutPassword})
+
+  }
+}
+
+export { SessionsController }
