7effb07f890c2ee22bf7ff51466f1de96f372906
CaioLima <caiohenrys50@gmail.com>
2026-08-13 13:45:11 -0300

    feat: routes verify authorization

--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { RefundsController } from "@/controllers/refunds-controller";
+import { verifyUserAuthorization } from "@/middleware/verify-user-authorization";
+import { UserRole } from "@prisma/client";
 
 const refundsRoutes = Router()
 
 const refundsController = new RefundsController()
 
-refundsRoutes.post("/", refundsController.create)
+refundsRoutes.post("/", 
+  verifyUserAuthorization([UserRole.employer]),
+  refundsController.create
+)
+
+refundsRoutes.get("/", 
+  verifyUserAuthorization([UserRole.manager]),
+  refundsController.index
+)
 
 export { refundsRoutes }
